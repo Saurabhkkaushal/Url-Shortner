060f7d32e1007ca9ea705c12516c1722d1a7b617
saurabhkkaushal <saurabhkaushal17@gmail.com>
2019-01-29 14:59:39 +0530

    indentation

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -14,19 +14,17 @@ class UrlsController < ApplicationController
   	if @url.present? && @url.domain_name == params[:url][:domain_name] #!= nil
       puts "url.present"
       respond_to do |format|
-            format.html {flash[:message] = "long url with same value already present and shorturl is: " + $domain_name +@url.short_domain + '/' + @url.short_url 
-              redirect_to urls_path  } 
-
-            format.json  { render json: {"shorturl" => $domain_name + @url.short_url} }
-          end
+        format.html {flash[:message] = "long url with same value already present and shorturl is: " + $domain_name +@url.short_domain + '/' + @url.short_url 
+        redirect_to urls_path  } 
+        format.json  { render json: {"shorturl" => $domain_name + @url.short_url} }
+      end
 
     elsif @url.present? && @url.domain_name != params[:url][:domain_name]    
-       respond_to do |format|
-            format.html {flash[:message] = "short Url is present but domain name not matched please enter correct domain name" 
-              redirect_to urls_path  } 
-
-            format.json  { render json: {"shorturl"=> "short Url is present but domain name not matched please enter correct domain name"} }
-          end
+      respond_to do |format|
+        format.html {flash[:message] = "short Url is present but domain name not matched please enter correct domain name" 
+        redirect_to urls_path  } 
+        format.json  { render json: {"shorturl"=> "short Url is present but domain name not matched please enter correct domain name"} }
+      end
 
   	else
   		@url = Url.new(url_params)
@@ -35,27 +33,22 @@ class UrlsController < ApplicationController
 
       if @url.short_url == 'error'
         respond_to do |format|
-            format.html {flash[:message] = "sorry, Unable to convert"
-              redirect_to urls_path  } 
-
-            format.json  { render json: {"error"=>"sorry, Unable to convert"} }
-          end
+          format.html {flash[:message] = "sorry, Unable to convert"
+          redirect_to urls_path  } 
+          format.json  { render json: {"error"=>"sorry, Unable to convert"} }
+        end
 
   		elsif @url.save
         respond_to do |format|
-            @url.short_url = @url.short_domain + "/" + @url.short_url
-            format.html {redirect_to url_path(id: @url , short_url: @url.short_url) }
-
-            format.json  { render json: {"shorturl"=> $domain_name + @url.short_url} }
-          end
-  		else
-
-        respond_to do |format|
-            format.html {render 'urls/index' }
-
-            format.json  { render json: {"shorturl"=> "please input in correct format "} }
+          @url.short_url = @url.short_domain + "/" + @url.short_url
+          format.html {redirect_to url_path(id: @url , short_url: @url.short_url) }
+          format.json  { render json: {"shorturl"=> $domain_name + @url.short_url} }
         end
-  			
+  		else
+        respond_to do |format|
+          format.html {render 'urls/index' }
+          format.json  { render json: {"shorturl"=> "please input in correct format "} }
+        end
   		end
     end
   end
@@ -84,17 +77,15 @@ class UrlsController < ApplicationController
       @shorturl = @shorturl[17..len]
       @long_url = Url.caching_implementation_for_short_Url(@shorturl)
   	  if @long_url != nil 
-
-          respond_to do |format|
-            format.html {redirect_to urls_showlongurl_path(long_url: @long_url.long_url) }
-
-            format.json  { render  json: {"longurl"=> @long_url.long_url} }
-          end
+        respond_to do |format|
+          format.html {redirect_to urls_showlongurl_path(long_url: @long_url.long_url) }
+          format.json  { render  json: {"longurl"=> @long_url.long_url} }
+        end
   	  else
-           respond_to do |format|
-            format.html {redirect_to urls_error_path}
-            format.json  { render  json: {"longurl"=> "No mapping availabale"} }
-          end      
+        respond_to do |format|
+          format.html {redirect_to urls_error_path}
+          format.json  { render  json: {"longurl"=> "No mapping availabale"} }
+        end      
   	  end
     end
 
@@ -107,17 +98,9 @@ class UrlsController < ApplicationController
 
   end
 
-
-
-
-
   def error
 
   end
-
-
-
-
 
   def search
 
@@ -137,10 +120,8 @@ class UrlsController < ApplicationController
 
   private
 
-		def url_params
-
-			params.require(:url).permit(:long_url , :domain_name)
-
-		end
+    def url_params
+      params.require(:url).permit(:long_url , :domain_name)
+    end
 
 end
