f8be22db8de6bf07d4ad43093f0acd0f34e2bb47
saurabhkkaushal <saurabhkaushal17@gmail.com>
2019-01-29 15:21:14 +0530

    indent_model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,7 @@
 class Url < ApplicationRecord
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
-	after_create :start_background_processing
+  after_create :start_background_processing
   
   settings do
     mappings dynamic: false do
@@ -13,17 +13,16 @@ class Url < ApplicationRecord
   validates :long_url, url: true , uniqueness: true
   validates :long_url , presence: true 
   before_validation :ensure_long_url_has_a_value
-
   validates :short_url , presence: true
-
-	validates :domain_name , presence: true , length: { minimum: 4 }
-
-	def start_background_processing
+  validates :domain_name , presence: true , length: { minimum: 4 }
+  def start_background_processing
     HardWorker.perform_async  
 	end
 
-	def as_indexed_json(options={})
+  def as_indexed_json(options={})
+
 	 as_json(only: [:long_url, :short_url, :domain_name])
+
 	end
 
 
@@ -39,19 +38,17 @@ class Url < ApplicationRecord
       short_url = short_url_2[itr..itr + 7]
     	itr += 1
     end
-        if(Url.where(short_url:  short_url).first != nil)
-        	return "error"
-        end	
+    if(Url.where(short_url:  short_url).first != nil)
+      return "error"
+    end	
     return short_url
   end
 
-
-
   def self.domain_shortner(domain_name)
-		encode_domain = Digest::MD5.hexdigest domain_name
-		short_domain = encode_domain[1..4]
-		itr = 2 
-		while itr+3 <= encode_domain.length && Url.find_by_short_domain(short_domain) != nil && Url.find_by_short_domain(short_domain).domain_name != domain_name 
+    encode_domain = Digest::MD5.hexdigest domain_name
+    short_domain = encode_domain[1..4]
+    itr = 2 
+    while itr+3 <= encode_domain.length && Url.find_by_short_domain(short_domain) != nil && Url.find_by_short_domain(short_domain).domain_name != domain_name 
 			short_domain = encode_domain[itr..itr+3]
 			itr += 1
 			puts itr
@@ -59,26 +56,23 @@ class Url < ApplicationRecord
 		return short_domain
 	end
 
-
-
   def self.caching_implementation_for_long_Url(long_url)
-		return  Rails.cache.fetch("#{long_url}", expires_in: 15.minutes) do
-            		Url.find_by_long_url(long_url)     
-          			end
+    return Rails.cache.fetch("#{long_url}", expires_in: 15.minutes) do
+      Url.find_by_long_url(long_url)
     end
+  end
 
   def self.caching_implementation_for_short_Url(short_url)
     return Rails.cache.fetch("#{short_url}",expires_in: 15.minutes) do
- 				Url.find_by_short_url(short_url)
+      Url.find_by_short_url(short_url)
     end
   end	
 
-
-	private
-    def ensure_long_url_has_a_value
-      if long_url.nil?
-        self.long_url = long_url unless long_url.blank?
-      end
+  private
+  def ensure_long_url_has_a_value
+    if long_url.nil?
+      self.long_url = long_url unless long_url.blank?
     end
-
+  end
+  
 end
